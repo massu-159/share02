cd5a97d342a01a15e35a5c05a13f764d6260f6cd
massu-159 <magj.dada.37458@softbank.ne.jp>
2021-01-31 22:41:44 +0900

    second-commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,49 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
+import Profile from '../views/Profile.vue'
+import Detail from '../views/Detail.vue'
+import store from '../store/index'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true,
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'signUp',
+    component: SignUp
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile,
+    meta: {
+      requiresAuth: true,
+    }
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    component: Detail,
+    meta: {
+      requiresAuth: true,
+    },
+    props: true,
   }
 ]
 
@@ -25,5 +52,18 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth) && !store.state.auth) {
+    next({
+      path: "/",
+      query: {
+        redirect: to.fullPath,
+      },
+    });
+  } else {
+    next();
+  }
+});
 
 export default router
